58b23b1a907cad6fedb5ff27426afba2c80f9a80
MtThoas <matthias.pecquery@gmail.com>
2023-02-09 12:55:39 +0100

    Ajout Cryptage Decryptage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { Form, Input, TextArea, Button, Dropdown} from 'semantic-ui-react';
+import { Form, Input, TextArea, Button, Dropdown, Radio } from 'semantic-ui-react';
 import Swal from 'sweetalert2';
+import { useState } from 'react';
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
 
 const hashage = [
   {
@@ -38,22 +41,163 @@ const hashage = [
 ];
 
 const App = () => {
-  const handleOnSubmit = (e) => {
+  const [inputTypeCrypt, setInputTypeCrypt] = useState("text");
+  const [inputTypeDecrypt, setInputTypeDecrypt] = useState("text");
+  const [selectedOptionCrypt, setSelectedOptionCrypt] = useState("");
+  const [selectedOptionDecrypt, setSelectedOptionDecrypt] = useState("");
+
+  const handleOnSubmitCrypt = (e) => {
     e.preventDefault();
-    const message = e.target.elements.user_message.value;
-    const selectedOption = e.target.elements.options.value;
-    console.log(message, selectedOption);
+    
+    let messageCrypt;
+    if (inputTypeCrypt === "text") {
+      messageCrypt = e.target.elements.user_message.value;
+      if (!messageCrypt) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Le message ne peut pas être vide!'
+        });
+        return;
+      }
+    } else {
+      messageCrypt = e.target.elements.user_file.files[0];
+      if (!messageCrypt) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Veuillez sélectionner un fichier!'
+        });
+        return;
+      }
+    }
+
+    let resultCrypt = "none";
+    if(selectedOptionCrypt === "AES"){
+      resultCrypt = AES.encrypt(messageCrypt, "LA CLE");
+    }else if(selectedOptionCrypt === "SHA256"){
+      resultCrypt = SHA256(messageCrypt);
+    }
+
+
+    console.log(messageCrypt);
     Swal.fire({
       icon: 'success',
-      title: `Message: ${message} Sent Successfully with Option: ${selectedOption}`
-    })
-    e.target.reset()
+      title: `Message: ${messageCrypt} Sent Successfully with Option: ${selectedOptionCrypt}\n  LA CLE : ${resultCrypt}`
+    });
+    e.target.reset();
+  };
+
+  const handleOptionChangeCrypt = (e, { value }) => setSelectedOptionCrypt(value);
+  const handleOptionChangeDecrypt = (e, { value }) => setSelectedOptionDecrypt(value);
+
+
+  const handleOnSubmitDecrypt = (e) => {
+    e.preventDefault();
+    
+    let messageDecrypt;
+    if (inputTypeDecrypt === "text") {
+      messageDecrypt = e.target.elements.user_message.value;
+      if (!messageDecrypt) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Le message ne peut pas être vide!'
+        });
+        return;
+      }
+    } else {
+      messageDecrypt = e.target.elements.user_file.files[0];
+      if (!messageDecrypt) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Veuillez sélectionner un fichier!'
+        });
+        return;
+      }
+    }
+
+
+
+    Swal.fire({
+      icon: 'success',
+      title: `Message: ${messageDecrypt} Sent Successfully with Option: ${selectedOptionDecrypt} \n`
+    });
+    e.target.reset();
   };
 
   return (
-    <div className='App'>
-      <h1>Send Message</h1>
-      <Form onSubmit={handleOnSubmit}>
+    <>
+    <div className="Crypte1">
+      <h1> Application de cryptage </h1>
+      <Form onSubmit={handleOnSubmitCrypt}>
+        <Form.Field>
+          <Radio
+            label="Text"
+            name="inputType"
+            value="text"
+            checked={inputTypeCrypt === "text"}
+            onChange={(e, { value }) => setInputTypeCrypt(value)}
+          />
+          <Radio
+            label="File"
+            name="inputType"
+            value="file"
+            checked={inputTypeCrypt === "file"}
+            onChange={(e, { value }) => setInputTypeCrypt(value)}
+          />
+        </Form.Field>
+        {inputTypeCrypt === "text" ? (
+          <Form.Field
+            id='form-textarea-control-opinion'
+            control={TextArea}
+            label='Message'
+            name='user_message'
+            placeholder='Message…'
+            required
+          />
+        ) : (
+          <Form.Field>
+            <label>File</label>
+            <input type="file" name="user_file" required />
+          </Form.Field>
+        )}
+         <Dropdown
+          placeholder='Select Hash Algorithm'
+          fluid
+          selection
+          options={hashage}
+          onChange={handleOptionChangeCrypt}
+          value={selectedOptionCrypt}
+          // value={selectedOptionCrypt}
+        />
+        <Button id="submit" type="submit" color="green">
+          Submit
+        </Button>
+      </Form>
+    </div>
+
+    <div className="Crypte2">
+    <h1> Application de décryptage </h1>
+    <Form onSubmit={handleOnSubmitDecrypt}>
+      <Form.Field>
+        <Radio
+          label="Text"
+          name="inputTypeDecryptage"
+          value="text"
+          checked={inputTypeDecrypt === "text"}
+          onChange={(e, { value }) => setInputTypeDecrypt(value)}
+        />
+        <Radio
+          label="File"
+          name="inputType"
+          value="file"
+          checked={inputTypeDecrypt === "file"}
+          onChange={(e, { value }) => setInputTypeDecrypt(value)}
+        />
+      </Form.Field>
+      {inputTypeDecrypt === "text" ? (
         <Form.Field
           id='form-textarea-control-opinion'
           control={TextArea}
@@ -62,16 +206,29 @@ const App = () => {
           placeholder='Message…'
           required
         />
-          <Dropdown
-          placeholder='Select Friend'
+  ) : (
+    <Form.Field>
+      <label>File</label>
+      <input type="file" name="user_file" required />
+    </Form.Field>
+  )}
+ <Dropdown
+          placeholder='Select Hash Algorithm'
           fluid
           selection
           options={hashage}
+          onChange={handleOptionChangeDecrypt}
+          value={selectedOptionDecrypt}
         />
-        <Button type='submit' color='green'>Submit</Button>
-      </Form>
+  <Button id="submit" type="submit" color="green">
+    Submit
+  </Button>
+</Form>
     </div>
+</>
   );
-}
+};
 
 export default App;
+
+
